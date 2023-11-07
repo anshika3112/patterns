439df9088ce5662e68a388296aeef823e09192a5
Anshika Singhal <139126541+anshika3112@users.noreply.github.com>
2023-11-07 21:32:48 +0530

    Create diamondpattern.cpp

---
+++ diamondpattern.cpp
@@ -0,0 +1,47 @@
+    *
+   * *
+  * * *
+ * * * *
+* * * * *
+* * * * *
+ * * * *
+  * * *
+   * *
+    *
+//code
+#include<stdio.h>
+int main()
+{
+    int n,i,j;
+    cout<<"enter n";
+    cin>>n;
+    for(int i=0;i<n;i++)
+   {
+       //spaces
+       for(j=0;j<n-i-1;j++)
+       {
+       cout<<" ";
+       }
+       //stars
+       {
+           for(int j=0;j<i+1;j++)
+           cout<<"* ";
+       }
+       cout<<endl;
+   }
+   for(int i=0;i<n;i++)
+   {
+       //spaces
+       for(j=0;j<i;j++)
+       {
+       cout<<" ";
+       }
+       //stars
+       {
+           for(int j=0;j<n-i;j++)
+           cout<<"* ";
+       }
+       cout<<endl;
+   }
+    return 0;
+}
