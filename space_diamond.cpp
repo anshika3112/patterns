e21ac2b332686b00d16e341ad2cc4d0fae42fe0d
Anshika Singhal <139126541+anshika3112@users.noreply.github.com>
2023-11-07 22:34:38 +0530

    Update space_diamond.cpp

--- space_diamond.cpp
+++ space_diamond.cpp
@@ -7,6 +7,36 @@
 * * *       * * *
 * * * *    * * * *
 // code
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main;
+
+int main() {
+    int n,i,j;
+    cout << "Enter n: ";
+    cin >> n;
+     for(int i = 0; i < n; i++) {
+       // inverted pyramid;
+       for(j=0;j<n-i;j++)
+       cout<<"*";
+       // spaces pyramid
+       for(j=0;j<2*i+1;j++)
+       cout<<" ";
+       //mirror inverted pyramid
+       for(j=0;j<n-i;j++)
+       cout<<"*";
+       cout<<endl;
+    }
+    for(int i = 0; i < n; i++) {
+       // inverted pyramid;
+       for(j=0;j<i+1;j++)
+       cout<<"*";
+       // spaces pyramid
+       for(j=0;j<2*n-2*i-1;j++)
+       cout<<" ";
+       //mirror inverted pyramid
+       for(j=0;j<i+1;j++)
+       cout<<"*";
+       cout<<endl;
+    }
+    return 0;
+}
