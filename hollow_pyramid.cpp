86696d3a1d2acb8db46028da9fe1fe6d7a3cfce9
Anshika Singhal <139126541+anshika3112@users.noreply.github.com>
2023-11-07 22:02:02 +0530

    Update hollow_pyramid.cpp

--- hollow_pyramid.cpp
+++ hollow_pyramid.cpp
@@ -6,3 +6,23 @@
 //code
 #include<iostream>
 using namespace std;
+int main() {
+    int n;
+    cout << "Enter n: ";
+    cin >> n;
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n - i - 1; j++) {
+            cout << " ";
+        }
+        for(int j = 0; j < 2*i+1; j++) {
+            if((j==0)||(j==i*2))
+            cout << "*";
+            else
+            cout<<" ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
