5597c9a17dbf4c5f0308bf708e742eb1da573af9
Anshika Singhal <139126541+anshika3112@users.noreply.github.com>
2023-11-07 22:47:23 +0530

    Update numbers_and_stars__side_tirangle.cpp

--- numbers_and_stars__side_tirangle.cpp
+++ numbers_and_stars__side_tirangle.cpp
@@ -3,19 +3,25 @@
 3 * 3 * 3
 4 * 4 * 4
 //code
-  #include <iostream>
+#include <iostream>
 using namespace std;
 
 int main() {
     int n,i,j;
     cout << "Enter n: ";
     cin >> n;
-     for(int i = 1; i <= n; i++) {
+     for(int i = 0; i < n; i++) {
        // inverted pyramid;
-       for(j=1;j<=i;j++)
-       cout<<i <<"*" ;
+       for(j=0;j<2*i+1;j++)
+       {
+           if(j%2==1)
+           cout<<" * ";
+           else
+           cout<<i+1;
+       }
         cout<<endl;
     }
    
     return 0;
 }
+
