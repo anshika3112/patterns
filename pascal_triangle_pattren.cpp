d01b45f20a4e2f85f93534ae86c8a6ab1fb95965
Anshika Singhal <139126541+anshika3112@users.noreply.github.com>
2023-11-11 18:40:08 +0530

    Update pascal_triangle_pattren.cpp

--- pascal_triangle_pattren.cpp
+++ pascal_triangle_pattren.cpp
@@ -1,1 +1,29 @@
-
+          1
+         1 1 
+        1 2 1
+       1 3 3 1
+      1 4 6 4 1
+    1 5 10 10 5 1 
+   1 6 15 20 15 6 1
+// code
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    {
+        for(int k=0;k<n-i;k++)
+        cout<<" ";
+        int c=1;
+        for(int j=1;j<=i;j++)
+        {
+           cout<<c<<" ";
+           c=c*(i-j)/j;
+        }
+        cout<<endl;
+    }
+    cout<<" ";
+    return 0;
+}
