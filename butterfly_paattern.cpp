dc7c33048c7cebee958b5b1e7b4be27d5e1096b8
Anshika Singhal <139126541+anshika3112@users.noreply.github.com>
2023-11-13 23:57:57 +0530

    Update butterfly_paattern.cpp

--- butterfly_paattern.cpp
+++ butterfly_paattern.cpp
@@ -1,1 +1,42 @@
+*                 *
+* *             * *
+* * *         * * *
+* * * *     * * * *
+* * * * * * * * * *
+* * * * * * * * * *
+* * * *     * * * *
+* * *         * * *
+* *             * *
+*                 *
+  //code
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    for(int i=0;i<2*n;i++)
+    {
+        int cond=i<n?i:n+(n-i-1);
+        int space=i<n?2*(n-cond-1):i-cond-1;
+        for(int j=0;j<2*n;j++)
+        {
+            if(j<=cond)
+            {
+                cout<<"*";
+            }
+            else if(space>0)
+            {
+                cout<<" ";
+                space--;
+            }
+            else
+            {
+                cout<<"*";
+            }
+        }
+        cout<<endl;
+    }
+    return 0;
+}
 
