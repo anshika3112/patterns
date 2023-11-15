f70b95831f217e5b7b860c8003c0ad5473af94e7
Anshika Singhal <139126541+anshika3112@users.noreply.github.com>
2023-11-16 00:04:44 +0530

    Update numeric pyramid.cpp

--- numeric pyramid.cpp
+++ numeric pyramid.cpp
@@ -5,7 +5,23 @@
 1  2  3  4
 //code
 #include<iostream>
+  using namespace std;
 int main()  
 {
-  
+    int n,j;
+    cout<<"enter the number"<<endl;
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        for( j=0;j<i+1;j++)
+        {
+           if((j==0)||(j==i)||(i==n-1))
+            cout<<j+1;
+            else
+            cout<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
 }
+
