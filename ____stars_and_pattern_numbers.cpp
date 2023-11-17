576d2ccb4d887206a38216db1fc6a1924ead6aae
Anshika Singhal <139126541+anshika3112@users.noreply.github.com>
2023-11-17 22:25:43 +0530

    Update ____stars_and_pattern_numbers.cpp

--- ____stars_and_pattern_numbers.cpp
+++ ____stars_and_pattern_numbers.cpp
@@ -1,1 +1,31 @@
-
+#include<iostream>
+using namespace std;                // stars number+star star
+// * * * * * * * * 1 * * * * * * * *    8      1          8
+// * * * * * * * 2 * 2 * * * * * * *    7     2+1         7
+// * * * * * * 3 * 3 * 3 * * * * * *    6     3+2         6
+// * * * * * 4 * 4 * 4 * 4 * * * * *    5     4+3         5
+// * * * * 5 * 5 * 5 * 5 * 5 * * * *    4     5+4         4
+int main()
+{
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        int start=8-i;
+        int num=i+1;
+        int count=num;
+        for(int j=0;j<17;j++)
+        {
+          if(j==start && count>0)
+          {
+              cout<<num<<" ";
+              start+=2; 
+              count--;
+          }
+          else
+          cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
+  
